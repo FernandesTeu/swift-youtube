0d722d26ae3d438611b80a0e63819b2b93198176
Mateus Fernandes Pinto <mateus.pinto@bancointer.com.br>
2021-06-11 15:20:36 -0300

    lutando pra aprender auto layout

--- estudosfinanceirocasa/estudosfinanceirocasa/ViewController.swift
+++ estudosfinanceirocasa/estudosfinanceirocasa/ViewController.swift
@@ -11,9 +11,17 @@ class ViewController: UIViewController {
 
      lazy var dashboard: UIView = {
         let view = UIView()
-        view.backgroundColor = .white
+        view.backgroundColor = .systemPink
+        view.translatesAutoresizingMaskIntoConstraints = true
         return view
     }()
+    
+    lazy var saldo: UIView = {
+       let view = UIView()
+        view.backgroundColor = .systemTeal
+       view.translatesAutoresizingMaskIntoConstraints = true
+       return view
+   }()
     
     lazy var cards: UIView = {
         let view = UIView()
@@ -24,6 +32,12 @@ class ViewController: UIViewController {
     lazy var schedule: UIView = {
         let view = UIView()
         view.backgroundColor = .clear
+        return view
+    }()
+    
+    lazy var teste: UIView = {
+        let view = UIView()
+        view.backgroundColor = .purple
         return view
     }()
     
@@ -43,7 +57,26 @@ class ViewController: UIViewController {
         return stack
     }()
     
-   
+    private lazy var interStack: UIStackView = {
+        let stack = UIStackView(frame: self.view.bounds)
+        stack.translatesAutoresizingMaskIntoConstraints = true
+        stack.alignment = .fill
+        stack.distribution = .fillEqually
+        stack.axis = .vertical
+        stack.spacing = 5
+        stack.addArrangedSubview(dashboard)
+        return stack
+    }()
+    
+    
+    lazy var month: UILabel = {
+        let title = UILabel(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height))
+        title.text = "Junho/21"
+        title.translatesAutoresizingMaskIntoConstraints = true
+        title.sizeToFit()
+        title.tintColor = .white
+        return title
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -51,20 +84,27 @@ class ViewController: UIViewController {
         setUpNavigationBar()
         view.backgroundColor = .white
         view.addSubview(stackView)
-        stackView.addArrangedSubview(dashboard)
+        stackView.addArrangedSubview(interStack)
         stackView.addArrangedSubview(cards)
         stackView.addArrangedSubview(schedule)
+        dashboard.addSubview(month)
         
-        
-        
-        
-        
+        NSLayoutConstraint.activate([
+            interStack.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            month.topAnchor.constraint(equalTo: self.view.topAnchor),
+            month.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            month.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            month.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
+            month.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: 20)
+        ])
+       
     }
     
     func setUpNavigationBar() {
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationController?.navigationBar.sizeToFit()
         navigationItem.title = "Dashboard"
+        navigationController?.navigationBar.isTranslucent = false
     }
 
     
